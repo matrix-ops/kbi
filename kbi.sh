f78755020ef6568b1ab69951649a102fbbc5557a
matrix-ops <58019420+matrix-ops@users.noreply.github.com>
2021-05-11 00:40:58 +0800

    Update kbi.sh
    update

--- kbi.sh
+++ kbi.sh
@@ -234,6 +234,8 @@ echo -e "\033[32mNTP服务器完成..........\033[0m"
 
 deployHaproxyKeepalived (){
 #生成Haproxy的配置文件，默认使用MasterIP中的前三个节点
+for i in ${MasterIP[*]};do ssh $i useradd keepalived_script;done
+for i in ${MasterIP[*]};do ssh $i "echo 'keepalived_script ALL = (root) NOPASSWD:ALL' > /etc/sudoers.d/keepalived_script";done
 cat << EOF > /tmp/haproxy.cfg 
 global
     log /dev/log    local0
@@ -285,7 +287,7 @@ global_defs {
 }
     
 vrrp_script check-haproxy {
-    script "killall -0 haproxy"
+    script "sudo killall -0 haproxy"
     interval 5
     weight -30
 }
